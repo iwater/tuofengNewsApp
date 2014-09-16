aaa32314a7c8b90e124b302c2abc53f8892fa5ce
Ma Tao <iwater@gmail.com>
2014-09-16 10:45:02 +0800

    fix some layout warning, TODO: detail view scroll bug after change textview

--- caijing/DetailViewController.swift
+++ caijing/DetailViewController.swift
@@ -18,8 +18,10 @@ func lend<T where T:NSObject> (closure:(T)->()) -> T {
 class DetailViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     var tableData:[JSONValue] = []
     var news:JSONValue!
+    var newsDetail:JSONValue?
     let newsHelper = NewsHelper()
     
+    @IBOutlet weak var scroller: UIScrollView!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var contentView: UITextView!
     @IBOutlet weak var timeLabel: UILabel!
@@ -34,42 +36,9 @@ class DetailViewController: UIViewController, UITableViewDelegate, UITableViewDa
         titleLabel.text = news["title"].string
         timeLabel.text = news["publish_time"].string
         sourceLabel.text = news["source"].string
-        //contentView.text = news!["long_summary"]! as String
         
-        
-        if let s2 = news!["long_summary"].string {
-            let fontSize:CGFloat = 17.0
-        
-        var content : NSMutableAttributedString!
-        content = NSMutableAttributedString(string:s2, attributes: [
-            NSFontAttributeName: UIFont(name:"HelveticaNeue-Light", size: fontSize)
-            ])
-        /*content.addAttributes([
-            //NSFontAttributeName: UIFont(name:"HelveticaNeue-Light", size:24),
-            NSExpansionAttributeName: 0.3,
-            NSKernAttributeName: -4 // negative kerning bug fixed in iOS 8
-            ], range:NSMakeRange(0,1))*/
-        
-        let s1:NSString = s2
-        
-        content.addAttribute(NSParagraphStyleAttributeName,
-            value:lend() {
-                (para2 : NSMutableParagraphStyle) in
-                para2.headIndent = -5
-                para2.firstLineHeadIndent = 2 * fontSize
-                //para2.tailIndent = -10
-                para2.lineBreakMode = .ByWordWrapping
-                para2.alignment = .Justified
-                para2.lineHeightMultiple = 1.1
-                para2.hyphenationFactor = 1.0
-                para2.paragraphSpacing = 15.0
-                para2.paragraphSpacingBefore = 0.0
-            }, range:NSMakeRange(0, s1.length))
-        
-            contentView.attributedText = content
-            contentView.tintColor = ColorHelper.UIColorFromRGB(0x1f1f1f)
-            contentView.scrollEnabled = false
-            contentView.contentInset.left = 50
+        if let summary = news!["long_summary"].string {
+            self.displaySummary(summary)
         }
     }
     
@@ -77,13 +46,59 @@ class DetailViewController: UIViewController, UITableViewDelegate, UITableViewDa
         super.init(coder: aDecoder)
     }
     
+    func displaySummary(summary: String){
+            let fontSize:CGFloat = 17.0
+            
+            var content : NSMutableAttributedString!
+            content = NSMutableAttributedString(string:summary, attributes: [
+                NSFontAttributeName: UIFont(name:"HelveticaNeue-Light", size: fontSize)
+                ])
+            /*content.addAttributes([
+            //NSFontAttributeName: UIFont(name:"HelveticaNeue-Light", size:24),
+            NSExpansionAttributeName: 0.3,
+            NSKernAttributeName: -4 // negative kerning bug fixed in iOS 8
+            ], range:NSMakeRange(0,1))*/
+            
+            let s1:NSString = summary
+            
+            content.addAttribute(NSParagraphStyleAttributeName,
+                value:lend() {
+                    (para2 : NSMutableParagraphStyle) in
+                    para2.headIndent = -5
+                    para2.firstLineHeadIndent = 2 * fontSize
+                    //para2.tailIndent = -10
+                    para2.lineBreakMode = .ByWordWrapping
+                    para2.alignment = .Justified
+                    para2.lineHeightMultiple = 1.1
+                    para2.hyphenationFactor = 1.0
+                    para2.paragraphSpacing = 15.0
+                    para2.paragraphSpacingBefore = 0.0
+                }, range:NSMakeRange(0, s1.length))
+            
+            contentView.attributedText = content
+            contentView.tintColor = ColorHelper.UIColorFromRGB(0x1f1f1f)
+            contentView.scrollEnabled = false
+            contentView.contentInset.left = 50
+    }
+    
     func update(json:JSONValue) {
-        self.news = json
+        self.newsDetail = json
+        println(newsDetail)
+        if let summary:String = newsDetail!["newsDetail"]["article"]["summary"].string {
+            self.displaySummary(summary)
+            println(summary)
+            println("hit")
+            println(self.contentView.contentSize)
+            self.contentView.frame = CGRectMake(self.contentView.frame.origin.x, self.contentView.frame.origin.y, self.contentView.contentSize.width, self.contentView.contentSize.height);
+            self.view.setNeedsLayout()
+            view.layoutSubviews()
+            println(self.scroller.contentSize)
+            println(self.scroller.scrollEnabled)
+            self.scroller.contentSize = CGSizeMake(self.scroller.frame.size.width, self.contentView.frame.origin.y + self.contentView.frame.size.height)
+        }
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        //println("hits")
-        //println(self.tableData)
         return tableData.count
     }
     
