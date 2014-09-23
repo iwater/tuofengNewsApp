da2183fadb59b29eb7bc701bc5df04ed6cd0ef84
iwater <iwater@gmail.com>
2014-09-23 15:47:01 +0800

    tunning offsets on title view

--- caijing/TitleView.swift
+++ caijing/TitleView.swift
@@ -15,23 +15,24 @@ class TitleView: UIView {
         self.backgroundColor = UIColor.clearColor()
         
         var label = UILabel(frame: CGRectMake(16, 16, 100, 25))
-        label.text = "· " + title
+        label.text = title
         label.font = UIFont(name:"Heiti-SC", size: 14)
         label.textColor = UIColor.whiteColor()
         
-        var iv = UIImageView(image: UIImage(named: "tf"))
-        var cr = UIView(frame: CGRectMake(0, 0, 40, 21))
+        var iv = UIImageView(image: UIImage(named: "logo"))
+        iv.frame = CGRectMake(2.5, 1.5, 33, 15)
+        
+        var cr = UIView(frame: CGRectMake(0, 0, 39, 19))
         cr.backgroundColor = UIColor.whiteColor()
         cr.layer.borderColor = UIColor.whiteColor().CGColor
-        cr.layer.borderWidth = 2
-        cr.layer.cornerRadius = 2
-        iv.frame = CGRectMake(2, 2, 36, 17)
+        cr.layer.borderWidth = 1.5
+        cr.layer.cornerRadius = 1.5
+        
         cr.addSubview(iv)
         
         self.addSubview(cr)
         self.addSubview(label)
         
-        //iv.setTranslatesAutoresizingMaskIntoConstraints(false)
         cr.setTranslatesAutoresizingMaskIntoConstraints(false)
         label.setTranslatesAutoresizingMaskIntoConstraints(false)
         
@@ -41,23 +42,30 @@ class TitleView: UIView {
         
         self.addConstraints(
             NSLayoutConstraint.constraintsWithVisualFormat(
-                "V:|-[title]-|", options: nil, metrics: nil, views: viewsDict))
+                "V:|-2-[title]", options: nil, metrics: nil, views: viewsDict))
         self.addConstraints(
             NSLayoutConstraint.constraintsWithVisualFormat(
-                "V:[logo(21)]", options: nil, metrics: nil, views: viewsDict))
+                "V:[logo(19)]", options: nil, metrics: nil, views: viewsDict))
         self.addConstraints(
             NSLayoutConstraint.constraintsWithVisualFormat(
-                "H:[logo(40)]", options: nil, metrics: nil, views: viewsDict))
+                "H:[logo(39)]", options: nil, metrics: nil, views: viewsDict))
         self.addConstraints(
             NSLayoutConstraint.constraintsWithVisualFormat(
                 "V:|-2-[logo]", options: nil, metrics: nil, views: viewsDict))
         self.addConstraints(
             NSLayoutConstraint.constraintsWithVisualFormat(
-                "H:|-[logo]-[title]", options: nil, metrics: nil,
+                "H:|-0-[logo]-12-[title]", options: nil, metrics: nil,
                 views: viewsDict))
     }
 
     required init(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func drawRect(rect: CGRect) {
+        super.drawRect(rect)
+        var context = UIGraphicsGetCurrentContext()
+        UIColor.whiteColor().setFill()
+        CGContextFillRect(context, CGRectMake(45, 10, 2.5, 2.5))
     }
 }
